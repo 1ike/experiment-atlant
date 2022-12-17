1bae3d21cc71d6f93a65d38b67e14b9ee9276778
1ike <googlohren@gmail.com>
2022-12-18 00:43:30 +0300

    Add oauth via google (tested on Expo).

--- src/features/auth/components/WelcomeAndRegLayout/Social/index.tsx
+++ src/features/auth/components/WelcomeAndRegLayout/Social/index.tsx
@@ -1,6 +1,8 @@
 import { StyleProp, ViewStyle } from 'react-native';
 
 import { makeStyles, SocialIcon, SocialMediaType } from '@rneui/themed';
+import * as Google from 'expo-auth-session/providers/google';
+import { useEffect } from 'react';
 import { Text, View } from '$ui';
 
 
@@ -8,6 +10,33 @@ type SocialProps = { containerStyle?: StyleProp<ViewStyle> };
 
 export default function Social({ containerStyle }: SocialProps) {
   const styles = useStyles();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '198119258557-mut8d8rlcbu51vsird091481der5g37u.apps.googleusercontent.com',
+    androidClientId: '198119258557-586vgk1dbag6v252s3h8653r4ol44hot.apps.googleusercontent.com',
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+
+      if (authentication?.accessToken) {
+        // eslint-disable-next-line no-inner-declarations
+        async function getUser() {
+          const responseUserData = await fetch(
+            'https://www.googleapis.com/oauth2/v1/userinfo',
+            { headers: { Authorization: `Bearer ${authentication!.accessToken}` } },
+          );
+          const userData = (await responseUserData.json()) as { email: string, name: string };
+          console.log('emailData = ', userData);
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        getUser();
+      }
+    }
+  }, [response]);
+
 
   return (
     <View style={[styles.container, containerStyle]}>
@@ -23,7 +52,9 @@ export default function Social({ containerStyle }: SocialProps) {
             type={type}
             // eslint-disable-next-line react/no-array-index-key
             key={i}
-            onPress={() => console.log('type = ', type)}
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onPress={() => promptAsync()}
+            disabled={!request}
           />
         ))}
       </View>
